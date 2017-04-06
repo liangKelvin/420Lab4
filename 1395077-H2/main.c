e47c48f22062be932704004174f3ab7622d8838b
Kelvin Liang <kelvinliang@uws107337usr.uws.ualberta.ca>
2017-04-06 14:19:01 -0600

    done

---
+++ 1395077-H2/main.c
@@ -0,0 +1,129 @@
+
+#define LAB4_EXTEND
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Lab4_IO.h"
+#include "timer.h"
+#include "mpi.h"
+
+#define EPSILON 0.00001
+#define DAMPING_FACTOR 0.85
+#define THRESHOLD 0.0001
+
+int main (int argc, char* argv[]){
+
+	struct node *nodehead;
+	int nodecount;
+	int *num_in_links, *num_out_links;
+	double *r, *r_pre;
+	double *r_local;
+	int i, j;
+	double damp_const;
+	int iterationcount = 0;
+	double start;
+	double end;
+	int myRank;
+	int size;
+
+	MPI_Init(NULL, NULL);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (get_node_stat(&nodecount, &num_in_links, &num_out_links)) return 254;
+	
+	int lowBound = nodecount*myRank / size;
+	int highBound = (myRank+1)*nodecount / size;
+	int nodecount_local = nodecount / size; 
+
+	// Calculate the result
+	if (node_init(&nodehead, num_in_links, num_out_links, lowBound, highBound)) return 254;
+	
+	r = malloc(nodecount * sizeof(double));
+	r_pre = malloc(nodecount * sizeof(double));
+	r_local = malloc(nodecount_local * sizeof(double));
+	for ( i = 0; i < nodecount; ++i)
+		r[i] = 1.0 / nodecount;
+	damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
+   
+
+	GET_TIME(start);
+	// CORE CALCULATION
+	do{
+		++iterationcount;
+		vec_cp(r, r_pre, nodecount);
+
+		for ( i = 0; i < nodecount_local; ++i){
+			r_local[i] = 0;
+			for ( j = 0; j < nodehead[i].num_in_links; ++j)
+				r_local[i] += r_pre[nodehead[i].inlinks[j]] / num_out_links[nodehead[i].inlinks[j]];
+			r_local[i] *= DAMPING_FACTOR;
+			r_local[i] += damp_const;
+		}
+		
+		// Version 3
+		MPI_Allgather(r_local, nodecount_local, MPI_DOUBLE, r, nodecount_local, MPI_DOUBLE, MPI_COMM_WORLD);
+
+		// Version 2
+		/*
+		int i;
+		
+		if(myRank == 0) {
+		   for(i = 0; i < nodecount_local; i++) {
+			r[i] = r_local[i];
+		   }
+		   for(i = 1; i < size; i++ ) {
+			MPI_Recv(&r[i*nodecount_local], nodecount_local, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			//MPI_Send(r, nodecount, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+		   }	
+		} else {
+		   MPI_Send(r_local, nodecount_local, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		}
+		MPI_Bcast(r, nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		*/
+		
+		
+		// Version 1
+		/*
+		int i;
+		
+		if(myRank == 0) {
+		   for(i = 0; i < nodecount_local; i++) {
+			r[i] = r_local[i];
+		   }
+		   for(i = 1; i < size; i++ ) {
+			MPI_Recv(&r[i*nodecount_local], nodecount_local, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		   }	
+			
+		    for(i = 1; i < size; i++) {
+                	MPI_Send(r, nodecount, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+            	    }
+		} else {
+		   MPI_Send(r_local, nodecount_local, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		   MPI_Recv(r, nodecount, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+		*/
+		
+
+	}while(rel_error(r, r_pre, nodecount) >= EPSILON);
+
+	
+	//printf("Program converges at %d th iteration.\n", iterationcount);
+	MPI_Finalize();
+	GET_TIME(end);
+	printf("time: %f\n", end-start);
+
+	if(myRank == 0) {
+		Lab4_saveoutput(r, nodecount, end-start);
+	}
+
+        // post processing
+        node_destroy(nodehead, nodecount_local);
+        free(num_in_links); free(num_out_links);
+
+	return 0;
+}
+
+
